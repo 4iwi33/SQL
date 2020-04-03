abb6515cb26f11e6ac0cd8f72ec81eaf48f52bf1
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-03 18:31:29 +0300

    change

--- dz/reg-form-php-master/db.php
+++ dz/reg-form-php-master/db.php
@@ -1,7 +1,7 @@
 <?php
 
 $link = mysqli_connect(
-    "remotemysql.com", "KKXjdtbTlL", "njqNMeBa6p", "KKXjdtbTlL"
+    "remotemysql.com", "BfhbD1H6nh", "rowI2pAKLv", "BfhbD1H6nh"
     )
 or die("Не удалось подключиться к базе данных");
 
